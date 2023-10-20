de448bb78df0822c301049121b8369985c95b8a2
josemacassan <josemacassan@gmail.com>
2023-10-20 19:43:58 +0200

    Add lesson 8 and lesson 9

---
+++ python/lesson9/functions.py
@@ -0,0 +1,29 @@
+def hello_world():
+    print("Hello World!")
+
+hello_world()
+
+
+def sum(num1=0, num2=0):
+    if (type(num1) is not int or type(num2) is not int):
+        return 0
+    return num1 + num2
+
+total = sum()
+print(total)
+
+# Use *args in a function when we dont know how many arguments 
+# are gonna be passed to the function
+def multiple_items(*args):
+    print(args)
+    print(type(args))
+
+multiple_items("Dave", "John", "Sara")
+
+
+def mult_named_items(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+
+mult_named_items(first="Dave", last="Gray")  
